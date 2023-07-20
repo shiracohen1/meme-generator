151d42d78716b690ed81c3c3f933bbce4b060a94
shiracohen1 <shira050104@gmail.com>
2023-07-20 09:44:30 +0300

    memes

--- src/meme.js
+++ src/meme.js
@@ -1,13 +1,20 @@
 import React from "react";
+import MemesData from "./memesData"
 
 export default function Meme() {
+    let image;
+    function randomMeme() {
+        const memes = MemesData.data.memes;
+        let random = Math.floor(Math.random () * memes.length);
+        image = memes[random]["url"];
+    }
     return (
         <div className="meme">
             <div className="inputs">
                 <input type="text" className="first-input" placeholder="Top text"/>
                 <input type="text" className="second-input" placeholder="Bottom text"/>
             </div>
-            <button type="button" className="new-image">Get a new meme image  🖼</button>
+            <button type="button" className="new-image" onClick={ randomMeme }>Get a new meme image  🖼</button>
         </div>
     )
 }
